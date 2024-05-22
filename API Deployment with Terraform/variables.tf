27159335e2cc991cecacdf97473828f9dadc694a
Suraj Dev <120789150+Suraj01Dev@users.noreply.github.com>
2024-05-22 22:17:32 +0530

    Create variables.tf

---
+++ API Deployment with Terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "Public Subnet CIDR values"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "DevOps Project 1 VPC 1"
+}
+
+variable "cidr_public_subnet" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+}
+
+
+variable "eu_availability_zone" {
+  type        = list(string)
+  description = "Availability Zones"
+}
+
+variable "public_key" {
+  type        = string
+  description = "DevOps Project 1 Public key for EC2 instance"
+}
+
+variable "ec2_ami_id" {
+  type        = string
+  description = "DevOps Project 1 AMI Id for EC2 instance"
+}
+
+variable "ec2_user_data_install_apache" {
+  type        = string
+  description = "Script for installing the Apache2"
+}
